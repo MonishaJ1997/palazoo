d9cabc38b64456120b57476f074cf15ff44ad043
MonishaJ1997 <monishapython@vtscoders.com>
2025-10-27 17:06:17 +0530

    Remove STATICFILES_DIRS from settings.py
    Removed STATICFILES_DIRS configuration.

--- pet_website/petplazoo/settings.py
+++ pet_website/petplazoo/settings.py
@@ -21,9 +21,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
